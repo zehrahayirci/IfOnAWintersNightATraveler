83581313c13d7e7fdd2a5e8044dfb918805c50de
zehra.hayirci <zehra.hayirci@helmholtz-muenchen.de>
2021-01-02 19:27:19 +0100

    Text Class, 10 text analysis

--- WordCount.py
+++ WordCount.py
@@ -1,35 +1,42 @@
 import re
 from collections import Counter
+import random
 
-file_frequent = open('mostFrequent.txt','r')
-frequent=file_frequent.read().splitlines()
-#with open('texts/TellTaleHearth.txt') as f:
-with open('texts/TheLastLeaf.txt') as f:
-#with open('texts/TheCantervilleGhost.txt') as f:
-    passage = f.read()
+class Text():
+    def __init__(self, title, number):
+        self.title=title
+        self.number=number
+        file_frequent = open('mostFrequent.txt','r')
+        self.frequent=file_frequent.read().splitlines()
+        self.processTextFile()
 
-words = re.findall(r'\w+', passage)
+    def processTextFile(self):
+        file_name = 'texts/' + self.title + '.txt'
+        with open(file_name) as f:
+            passage = f.read()
+        passage = re.sub("\d+", " ", passage)
+        words = re.findall(r'\w+', passage)
+        cap_words = [word.upper() for word in words]
+        for element in self.frequent:
+            if element in cap_words:
+                cap_words = list(filter((element).__ne__, cap_words))
+        self.word_counts = Counter(cap_words)
 
-cap_words = [word.upper() for word in words]
-for element in frequent:
-    if element in cap_words:
-        cap_words = list(filter((element).__ne__, cap_words))
-#print(frequent)
-#print(cap_words)
+    def mostCommon(self):
+        print("Most common:")
+        s = []
+        for letter, count in self.word_counts.most_common(self.number):
+            print('%s: %7d' % (letter, count))
+            s.append(letter.lower())    
+        return s
 
+    def leastComon(self):
+        least_common=[]
+        print("least common:")
+        for word in self.word_counts:
+            if self.word_counts[word]==1:
+                least_common.append(word.lower())
 
-word_counts = Counter(cap_words)
-
-#print(word_counts)
-
-print("Most common:")
-for letter, count in word_counts.most_common(20):
-    print('%s: %7d' % (letter, count))
-
-print("least common:")
-for word in word_counts:
-    if word_counts[word]==1:
-        print(word)
-
-
-
+        small_list = random.sample(least_common, self.number)
+        print(small_list)
+        return small_list       
